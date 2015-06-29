5786bd3375fabc76d7fdfcfe5e801b28dcb380f1
Samson Brock <SamsonLB@gmail.com>
2015-06-29 15:46:08 -0600

    Extension: Guess Limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,18 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+def reset_game
+  @@guesses_remaining = 5
+  @@secret_number = rand(100)
+end
+
+def lost_game
+  reset_game
+  " You lost. A new number has been generated."
+end
 
 def check_guess(guess, secret_number)
   if guess
     if guess.to_i > (secret_number + 5)
+      @@guesses_remaining -= 1
       "Way too high!"
     elsif guess.to_i < (secret_number - 5)
+      @@guesses_remaining -= 1
       "Way too low!"
     elsif guess.to_i > secret_number
+      @@guesses_remaining -= 1
       "Too high!"
     elsif guess.to_i < secret_number
+      @@guesses_remaining -= 1
       "Too low!"
     else
+      reset_game
       "You got it right! The secret number is #{secret_number}."
     end
   else
@@ -20,13 +34,14 @@ def check_guess(guess, secret_number)
   end
 end
 
-set :secret_number, rand(100)
+reset_game
 
 get '/' do
   guess = params['guess']
-  message = check_guess(guess, settings.secret_number)
+  message = check_guess(guess, @@secret_number)
+  message += lost_game if @@guesses_remaining < 1
 
   erb :index, :locals =>
-                {:secret_number => settings.secret_number,
+                {:secret_number => @@secret_number,
                  :message => message}
 end
